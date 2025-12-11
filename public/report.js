800cc829fa96f99a585f3a93c7f1d18422a858fc
ismailaj <ismailaj664@gmail.com>
2025-12-11 10:32:47 +0100

    included assest folder

---
+++ public/report.js
@@ -0,0 +1,10 @@
+const optionOne = document.getElementById("optionOne");
+
+// const value = () => {
+
+// }
+optionOne.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(optionOne.value);
+    
+})
